dbfdbd769fc0679a19caadc67431fe4c4fa83371
Negesis23 <86001290+negesis23@users.noreply.github.com>
2023-12-16 20:39:14 +0700

    Update services.js

--- src/controller/services.js
+++ src/controller/services.js
@@ -94,12 +94,26 @@ const Services = {
                         endpoint,
                     })
                 })
-    
+
+                const current_page = parseInt($('.pagination .pagenavix .page-numbers.current').text());
+  const last_visible_page = parseInt($('.pagination .pagenavix .page-numbers:last').prev('a.page-numbers').text());
+  const next_page = current_page < last_visible_page ? current_page + 1 : null;
+  const previous_page = current_page > 1 ? current_page - 1 : null;
+  const has_next_page = current_page < last_visible_page;
+  const has_previous_page = current_page > 1;
+
                 return res.status(200).json({
                     status: true,
                     message: "success",
                     completed,
-                    currentPage: page
+                    pagination:{
+    current_page,
+    last_visible_page: current_page < last_visible_page ? last_visible_page : current_page,
+    has_next_page,
+    next_page,
+    has_previous_page,
+    previous_page    
+  }
                 })
             }
             return res.send({
